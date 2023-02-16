926ddaac64205525498f842fa1ddf6c7e7cb42b3
simonmakan <makansimon548@gmail.com>
2023-02-16 21:24:09 +0300

    last test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - program should compile without any warnings
- * Return: (0) (work done successfully)
+ * Return: (1) (work done successfully)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-       return (0);	
+       return (1);	
 }
