29af1247b600a9ffc05a9877605533c9b39ede4d
simonmakan <makansimon548@gmail.com>
2023-02-16 18:43:26 +0300

    proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - with proper grammar
+ * Return: 0 (project done)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
