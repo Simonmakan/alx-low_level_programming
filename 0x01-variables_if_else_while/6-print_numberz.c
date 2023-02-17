d88b8dbaf6d6015132f6e48488f117166dc575df
simonmakan <makansimon548@gmail.com>
2023-02-17 11:00:00 +0300

    no six task well done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - all single digits are printed
+ * Return: (0) (project done well no six)
+ */
+int main(void)
+{
+	int s;
+
+	s = 0;
+	while
+		(s < 10) {
+			putchar(s + '0');
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
