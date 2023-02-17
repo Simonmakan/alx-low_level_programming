ad6ec5d7bf764c013e2f9d67dc5a091b1272f5c8
simonmakan <makansimon548@gmail.com>
2023-02-17 14:43:39 +0300

    error to be corrected later

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - inventing in a combination of brain and materials
+ * Return: (0) (the project is done well)
+ */
+int main(void)
+{
+	int s;
+	int m;
+
+	m = 0;
+	while (m < 10)
+	{
+		s = 0;
+		while (s < 10)
+		{
+			if (m != s && m < s)
+			{
+				putchar('0' + s);
+				putchar('0' + m);
+
+				if (m + s != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			s++;
+		}
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
