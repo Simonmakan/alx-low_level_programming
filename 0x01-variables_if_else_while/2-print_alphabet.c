59f4d7429bb75683ca5f673b3918cbb176bb160e
simonmakan <makansimon548@gmail.com>
2023-02-17 09:15:24 +0300

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - alphabet game in lowercase
+ * Return: (0) (project done)
+ */
+int main(void)
+{
+	char s;
+
+	s = 'a';
+	while
+		(s <= 'z') {
+			putchar(s);
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
