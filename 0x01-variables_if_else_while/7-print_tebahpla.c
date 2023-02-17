afda8eece9051582975eaddfb1c2777baa5b351d
simonmakan <makansimon548@gmail.com>
2023-02-17 11:13:01 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-print_tebahpla.c -o 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabet in reverse format
+ * Return: (0) (program running well)
+ */
+int main(void)
+{
+	char s;
+
+	s = 'z';
+	while
+		(s >= 'a') {
+			putchar(s);
+			s--;
+		}
+	putchar('\n');
+	return (0);
+}
