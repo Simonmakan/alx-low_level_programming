25ac566d069d4e4693b609c3b2bb25e42bde74e6
simonmakan <makansimon548@gmail.com>
2023-02-21 07:40:37 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prog printing putchar and a new line
+ * Return: 0 (program is done well)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
