49c31893cda34b6571536e7f273b6aad2743270d
simonmakan <makansimon548@gmail.com>
2023-02-23 12:14:10 +0300

    tasks done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - A function who print the numbers:
+ * Return: Does not return anything
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
