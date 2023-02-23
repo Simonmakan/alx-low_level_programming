35f254e922de1935bb268cb8d7f4e9f83992bbf4
simonmakan <makansimon548@gmail.com>
2023-02-23 13:59:32 +0300

    lets fid prime no

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Finds and prints the largest prime factor of the number 612852475143
- *
- * Return: 0 if success
+ * main - prints largest p no
+ * Return: 0 (task done well)
  */
 int main(void)
 {
-	unsigned long num = 612852475143;
-	unsigned long factor = 2;
+unsigned long num = 612852475143;
+unsigned long factor = 2;
 
-	while (num)
-	{
-		if (num / factor > 0 && num % factor == 0)
-			num /= factor;
-		else if (num / factor > 0 && num % factor != 0)
-			factor++;
-		else
-			break;
-	}
-	printf("%ld\n", factor);
-	return (0);
+while (num)
+{
+if (num / factor > 0 && num % factor == 0)
+num /= factor;
+else if (num / factor > 0 && num % factor != 0)
+factor++;
+else
+break;
+}
+printf("%ld\n", factor);
+return (0);
 }
