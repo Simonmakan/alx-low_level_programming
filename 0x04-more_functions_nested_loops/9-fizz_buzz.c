f7e89af4530bcd8079226867fd7c1e0b18ed4ba8
simonmakan <makansimon548@gmail.com>
2023-02-23 13:55:26 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-fizz_buzz.c -o 9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 /**
- * main  - the entry point
- *
- * Return: 0 if the program run with success
+ * main  -fizz buzz
+ * Return: 0 (task done well)
  */
 int main(void)
 {
-	int a = 0, b = 0, c = 0;
+int s = 0;
+int m;
+int n;
 
-	for (a = 1; a < 101; a++)
-	{
-		b = a % 3;
-		c = a % 5;
-		if (b == 0 && c == 0)
-			printf("FizzBuzz");
-		else if (c == 0 && b != 0)
-			printf("Buzz");
-		else if (b == 0 && c != 0)
-			printf("Fizz");
-		else
-			printf("%d", a);
-		if (a < 100)
-			putchar(' ');
-	}
-	putchar('\n');
-	return (0);
+for (s = 1; s < 101; s++)
+{
+m = s % 3;
+n = s % 5;
+if (m == 0 && n == 0)
+printf("FizzBuzz");
+else if (n == 0 && m != 0)
+printf("Buzz");
+else if (m == 0 && n != 0)
+printf("Fizz");
+else
+printf("%d", s);
+if (s < 100)
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
