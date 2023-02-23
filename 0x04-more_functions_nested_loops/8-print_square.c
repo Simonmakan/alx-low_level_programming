524fefb1253ffc84969cccd005abe5fabe75ab88
simonmakan <makansimon548@gmail.com>
2023-02-23 13:38:35 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 8-main.c 8-print_square.c -o 8-squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
- * print_square - A function who print a square
- *
- * @size: size of square
- *
- * Return: Does not return anything
+ * print_square - print sq
+ * @size: area
+ * Return: (void)
  */
 void print_square(int size)
 {
-	int i, j;
-	int n = size;
+int s;
+int m;
+int n = size;
 
-	if (n > 0)
-	{
-		for (j = 0; j < n; j++)
-		{
-			for (i = 0; i < n; i++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
-		}
-	}
-	else
-		_putchar('\n');
+if (n > 0)
+{
+for (m = 0; m < n; m++)
+{
+for (s = 0; s < n; s++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
