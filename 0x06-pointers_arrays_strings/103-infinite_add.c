7944dab2aa5f6622970129124ca2cef16adf42a0
simonmakan <makansimon548@gmail.com>
2023-03-02 13:22:20 +0300

    infinit addition

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * rev_string - rev
+ * @n: int
+ * Return: (0 project done well)
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+void rev_string(char *n)
+{
+int s = 0, m = 0;
+char tmp;
+
+while (*(n + s) != '\0')
+{
+s++;
+}
+s--;
+for (m = 0; m < s; m++, s--)
+{
+tmp = *(n + m);
+*(n + m) = *(n + s);
+*(n + s) = tmp;
+}
+}
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int ovf = 0, s = 0, m = 0, dgts = 0;
+int v1 = 0, v2 = 0, tmpt = 0;
+
+while (*(n1 + s) != '\0')
+s++;
+while (*(n2 + m) != '\0')
+m++;
+s--;
+m--;
+if (m >= size_r || s >= size_r)
+return (0);
+while (m >= 0 || s >= 0 || ovf == 1)
+{
+if (s < 0)
+v1 = 0;
+else
+v1 = *(n1 + s) - '0';
+if (m < 0)
+v2 = 0;
+else
+v2 = *(n2 + m) - '0';
+tmpt = v1 + v2 + ovf;
+if (tmpt >= 10)
+ovf = 1;
+else
+ovf = 0;
+if (dgts >= (size_r - 1))
+return (0);
+*(r + dgts) = (tmpt % 10) + '0';
+dgts++;
+m--;
+s--;
+}
+if (dgts == size_r)
+return (0);
+*(r + dgts) = '\0';
+rev_string(r);
+return (r);
+}
