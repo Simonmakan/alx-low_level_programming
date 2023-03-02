ce768d6f4639d96f0158f2772dc49b5f3b8665fd
simonmakan <makansimon548@gmail.com>
2023-03-02 08:46:37 +0300

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - conc. no 
+ * @dest: string
+ * @src: parameter
+ * Return: dest
+**/
+char *_strcat(char *dest, char *src)
+{
+int dest_len = strlen(dest);
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+dest[dest_len + i] = src[i];
+dest[dest_len + i] = '\0';
+return (dest);
+}
