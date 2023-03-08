76830e749e04f97aad570d134f64d889025a4603
simonmakan <makansimon548@gmail.com>
2023-03-08 13:09:46 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89  2-main.c 2-strlen_recursion.c -o 2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen_recursion - length
+ *@s: pointer
+ *Return: (1) 
+ */
+int _strlen_recursion(char *s)
+{
+int i = 0;
+
+if (*s != 0)
+{
+i = _strlen_recursion(s + 1);
+return (++i);
+}
+return (i);
+}
