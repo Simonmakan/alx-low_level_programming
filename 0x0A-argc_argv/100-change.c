0357b3cfdba24245716754c2ead16ab337e9b4b9
simonmakan <makansimon548@gmail.com>
2023-03-10 13:22:43 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-change.c -o change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main func 
+ *Description: min no. of coins
+ *@argc: arguments
+ *@argv: array pointer
+ *Return: (min no.)
+ */
+int main(int argc, char *argv[])
+{
+int s = 0, m = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+m = atoi(argv[1]);
+while (m > 0)
+{
+if (m - 25 >= 0)
+{
+m = m - 25;
+}
+else if (m - 10 >= 0)
+{
+m = m - 10;
+}
+else if (m - 5 >= 0)
+{
+m = m - 5;
+}
+else if (m - 2 >= 0)
+{
+m = m - 2;
+}
+else if (m - 1 == 0)
+{
+m = m - 1;
+}
+s++;
+}
+printf("%d\n", s);
+return (0);
+}
