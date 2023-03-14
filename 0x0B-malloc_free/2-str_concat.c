db76f7133bfd0a1b678b2a3e8eb8e5c392e2571b
simonmakan <makansimon548@gmail.com>
2023-03-14 12:52:08 +0300

    concat correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,39 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *str_concat - conc strings
- *@s1: string1
- *@s2: string2
- *Return: (pointer/NULL)
+ * str_concat - concatenate strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL if failure
  */
 char *str_concat(char *s1, char *s2)
 {
-int m1, m2, length_1, length_2;
-char *s;
+	char *s;
+	int a, b, c, d;
 
-if (s1 == 0)
-{
-s1 = "";
-}
-for (length_1 = 0; s1[length_1] != 0; length_1++)
-if (s2 == 0)
-{
-s2 = "";
-}
-for (length_2 = 0; s2[length_2] != 0; length_2++)
-s = malloc(sizeof(char) * (length_1 + length_2 + 1));
-if (s == 0)
-{
-return (0);
-}
-for (m1 = 0; m1 < length_1; m1++)
-{
-s[m1] = s1[m1];
-}
-for (m2 = 0; m2 < length_2; m1++, m2++)
-{
-s[m1] = s2[m2];
-}
-s[m1] = 0;
-return (s);
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+	b++;
+	s = malloc(a * sizeof(*s1) + b * sizeof(*s2));
+	if (s == 0)
+		return (NULL);
+	for (c = 0, d = 0; c < a + b; c++)
+	{
+		if (c < a)
+			s[c] = s1[c];
+		else
+			s[c] = s2[d++];
+	}
+	return (s);
 }
