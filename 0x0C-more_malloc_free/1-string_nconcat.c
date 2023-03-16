ba2a8a6830fcc5b2910a35427fa7ec5adccc7f12
Simonmakan <117057507+Simonmakan@users.noreply.github.com>
2023-03-16 14:34:33 +0300

    Update 1-string_nconcat.c
    desturbed errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,28 +10,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int a, b, c, d;
+char *p;
+unsigned int s, m, k, l;
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	for (a = 0; s1[a] != '\0'; a++)
-		;
-	for (b = 0; s2[b] != '\0'; b++)
-		;
-	if (n > b)
-		p = malloc((a + b + 1) * sizeof(char));
-	else
-		p = malloc((a + n + 1) * sizeof(char));
-	if (p == 0)
-		return (NULL);
-
-	for (c = 0; c < a; c++)
-		p[c] = s1[c];
-	for (d = 0; d < n && d < b; d++, c++)
-		p[c] = s2[d];
-	p[c] = '\0';
-	return (p);
+if (s1 == 0)
+s1 = "";
+if (s2 == 0)
+s2 = "";
+for (s = 0; s1[s] != '\0'; s++)
+;
+for (m = 0; s2[m] != '\0'; m++)
+;
+if (n > m)
+ptr = malloc((s + b + 1) * sizeof(char));
+else
+ptr = malloc((s + n + 1) * sizeof(char));
+if (ptr == 0)
+return (NULL);
+for (k = 0; k < s; k++)
+ptr[k] = s1[k];
+for (l = 0; l < n && l < m; l++, k++)
+ptr[k] = s2[l];
+ptr[k] = '\0';
+return (ptr);
 }
