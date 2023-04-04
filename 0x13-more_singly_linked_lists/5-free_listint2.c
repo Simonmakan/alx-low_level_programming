11f7f577c421df95f37c712a279849e0411e896d
simonmakan <makansimon548@gmail.com>
2023-04-04 09:02:47 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 3-add_nodeint_end.c 0-print_listint.c 5-free_listint2.c -o f

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+* free_listint2 - frees a list
+* @head: pointer to the node
+*/
+
+
+void free_listint2(listint_t **head)
+{
+listint_t *cp;
+if (head == NULL)
+return;
+
+while (*head != NULL)
+{
+cp = *head;
+*head = cp->next;
+free(cp);
+}
+}
